ccb48500855f3279bee9a7847be67d089ec93007
Vitaly Kuzyaev <vitkuz573@gmail.com>
2026-07-19 20:21:16 +0500

    feat(13-01): register new vendor scrapers in SourceRegistry
    - Add imports for QualcommScraper, MediaTekScraper, IntelAMDScraper, AppleTechSpecsScraper
    - Update __all__ exports
    - All 4 scrapers auto-discovered by SourceRegistry.discover()

--- src/soc_db/scraping/__init__.py
+++ src/soc_db/scraping/__init__.py
@@ -12,23 +12,31 @@ from soc_db.scraping.rate_limit import PerSourceRateLimiter
 from soc_db.scraping.registry import SourceRegistry
 from soc_db.scraping.source import HTTPSource
 from soc_db.scraping.sources.apple import AppleScraper
+from soc_db.scraping.sources.apple_techspecs import AppleTechSpecsScraper
 from soc_db.scraping.sources.geekbench import GeekbenchScraper
+from soc_db.scraping.sources.intel_amd import IntelAMDScraper
 from soc_db.scraping.sources.linux_dt import LinuxDTScraper
+from soc_db.scraping.sources.mediatek import MediaTekScraper
 from soc_db.scraping.sources.notebookcheck import NotebookCheckScraper
+from soc_db.scraping.sources.qualcomm import QualcommScraper
 from soc_db.scraping.sources.techpowerup import TechPowerUpScraper
 from soc_db.scraping.sources.wikidata import WikidataScraper
 from soc_db.scraping.sources.wikipedia import WikipediaScraper
 
 __all__ = [
     "AppleScraper",
+    "AppleTechSpecsScraper",
     "BaseScraper",
     "ChipScrapeResult",
     "DriftReport",
     "GeekbenchScraper",
     "HTTPSource",
+    "IntelAMDScraper",
     "LinuxDTScraper",
+    "MediaTekScraper",
     "NotebookCheckScraper",
     "PerSourceRateLimiter",
+    "QualcommScraper",
     "SchemaDriftDetector",
     "SourceRegistry",
     "TechPowerUpScraper",
